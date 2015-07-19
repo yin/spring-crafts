a415316ad62f6c4f3fd98af7da0565628916351f
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2015-07-19 17:26:26 +0200

    Now added a singleton printer bean, which will make use of a future proxy to always reach most recent instance of the scoped injected bean

--- runtime/src/main/java/com/github/yin/spring/crafts/runtime/MainApp.java
+++ runtime/src/main/java/com/github/yin/spring/crafts/runtime/MainApp.java
@@ -6,17 +6,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		HelloWorldPrinter printer = (HelloWorldPrinter) context.getBean("printer");
+		printer.print();
+		printer.print();
+		printer.print();
+		printer.print();
 		checkBeanInstance2x(context, "helloWorldSingleton");
 		checkBeanInstance2x(context, "helloWorld");
 	}
-
-	private static void checkBeanInstance2x(ApplicationContext context, String bean) {
+	
+	public static void checkBeanInstance2x(ApplicationContext context, String bean) {
 		checkBeanInstance(context, bean);
 		checkBeanInstance(context, bean);
 	}
 
-	private static void checkBeanInstance(ApplicationContext context, String bean) {
+	public static void checkBeanInstance(ApplicationContext context, String bean) {
 		HelloWorld obj = (HelloWorld) context.getBean(bean);
 		System.out.println(bean + " >> " + obj.getClass().getSimpleName() + "#" + obj.hashCode());
 	}
+
 }
--- runtime/src/test/java/com/github/yin/spring/crafts/runtime/HelloWorldTest.java
+++ runtime/src/test/java/com/github/yin/spring/crafts/runtime/HelloWorldTest.java
@@ -8,20 +8,30 @@ import org.junit.Test;
 public class HelloWorldTest {
 
 	@Test
-	public void test_getMessage() {
+	public void test_print() {
 		// Mocking out the stdout
 		System.setOut(new PrintStream(System.out) {
 			@Override
 			public void println(String msg) {
 				super.println(msg);
 				// Performing the check in the mock, :(
-				Assert.assertEquals("Should print Message previously set", "Your Message: Test msg", msg);
+				Assert.assertEquals("Should print Message previously set", "Test msg", msg);
 			}
 		});
-		HelloWorld tested = new HelloWorld();
-		tested.setMessage("Test msg");
+		HelloWorldPrinter tested = new HelloWorldPrinter();
+		HelloWorld hw = new HelloWorld();
+		tested.setHelloWorld(hw);
+		hw.setMessage("Test msg");
 		// Executing the test, effectively
-		tested.getMessage();
+		tested.print();
+	}
+
+	@Test
+	public void test_getMessage() {
+		HelloWorld tested = new HelloWorld();
+		tested.setMessage("Test msg 2");
+		// Executing the test, effectively
+		Assert.assertEquals("shoult return set message", "Test msg 2", tested.getMessage());
 	}
 
 }
